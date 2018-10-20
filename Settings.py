f94dbe3049c2d42f9b4bdf5bdfda1db52737a6fa
Steven Kundert <stevenkundert@yahoo.com>
2018-10-20 14:15:40 -0500

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,6 +1,6 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
 PASS = "" # your Twitch OAuth token
-		  # Redacted so no one hijacks my bot
+	  # Redacted so no one hijacks my bot
 IDENT = "bravenewfavesbot"  # Twitch username you're using for your bot
 CHANNEL = "kathleen_lrr"   # Channel

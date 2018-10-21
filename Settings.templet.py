5a567ebfa08215a90c3c6456dc3b16e659b6a10a
Andrew Brown <ttle28c@gmail.com>
2018-10-20 21:16:11 -0700

    Set settings to sane defaults or empty strings

--- Settings.templet.py
+++ Settings.templet.py
@@ -1,7 +1,9 @@
+# Example settings file.
+# Fill out these values and copy to settings.py
 HOST = "irc.twitch.tv"
 PORT = 6667
 PASS = "" # your Twitch OAuth token
 	  # Redacted so no one hijacks my bot
-IDENT = "bravenewfavesbot"  # Twitch username you're using for your bot
+IDENT = ""  # Twitch username you're using for your bot
 CHANNEL = "kathleen_lrr"   # Channel
 ADMIN = ""	# User with access to bot

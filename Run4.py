ed841ed0f426a5c724a3268e38b2bbfd2939d7e8
Steven Kundert <stevenkundert@yahoo.com>
2019-02-02 22:53:36 -0600

    Add files via upload

---
+++ Run4.py
@@ -0,0 +1,150 @@
+#BraveNewFavesBot chat bot code
+#Most of this code is from a tutorial, so I hope there isn't anything dumb in here
+import string
+from Read import getUser, getMessage
+from Socket import openSocket, sendMessage
+from Initialize import joinRoom
+import datetime
+import io
+
+#This will open up a socket and print a nice message to the chat saying the bot is working
+s = openSocket()
+joinRoom(s)
+
+readbuffer = "" #buffer for incoming chat messages
+lst = [] 		#start a list for user submissions
+playedlst = [] 	#start a list for the requests that have been played
+reqcount = 0 	#index to keep track of the order songs were requested
+
+#Here I come up with unique names for text files to be created by the bot
+# so that it doesn't write over older lists.
+fname = datetime.datetime.now().strftime("%m-%d-%H-%M")
+reqlist = fname + '-reqlist.txt'
+playlist = fname + '-playlist.txt'
+
+#Error message if someone tries typing !spotify before pterodactal sets it
+spotifylist = 'No spotify playlist set'
+
+while True:
+		#Get a message from chat, then clean it up for our purposes
+		readbuffer = readbuffer + s.recv(1024).decode("utf-8")
+		temp = str.split(readbuffer, "\n")
+		readbuffer = temp.pop()
+		
+		for line in temp:
+			print(line)
+			#Gotta PONG when twitch PINGs you, or you'll lose your socket
+			#This is probably a pretty bad way of writing this but it works
+			if "PING :tmi.twitch.tv" in line:
+				s.send("{}\r\n".format("PONG :tmi.twitch.tv").encode("utf-8"))
+				break
+			user = getUser(line)
+			message = getMessage(line)
+			#Prints a cleaned-up version of every message typed in chat to the prompt screen,
+			# even though all that information already gets displayed
+			print (user + " :" + message) 
+			
+			#This will check each message in chat to see if it has a youtube link in it,
+			# then add the post to the list and the bot's chat if it does.
+			# I bet I could clean this up but it picks up all the messages in the format
+			# with which viewers have been submitting videos in the past.
+			#I added a counter to make it easier to tell which request is being played and so we 
+			# can easily access it in the list later. Ultimately we will want the bot to print the 
+			# request currently playing, so I don't want the bot to pick up its own messages.
+			if ("youtube.com" in message or "youtu.be" in message) and ("bravenewfavesbot" not in user) and ("playlist" not in message):
+				#Add the post to the list
+				reqcount = reqcount + 1
+				lst.append(str(reqcount) + ": " + user + ": " + message)
+				#Add the post to the bot's chat. Might make this its own function at some point,
+				# especially since it should print to whatever the bot's name is rather than this
+				# bot specifically.
+				messageTemp = "PRIVMSG #" + "bravenewfavesbot" + " :" + str(reqcount) + ": " + user + ": " + message
+				s.send("{}\r\n".format(messageTemp).encode("utf-8"))
+				break
+				
+				
+			#When command !howto is typed in chat, will print instructions for requesting a song	
+			if "!howto" in message:
+				messageTemp = "To request a song for Brave New Faves, wait until Kathleen asks for suggestions during "
+				messageTemp = messageTemp + "the last song, then drop a YouTube link in chat, along with the artist's name, "
+				messageTemp = messageTemp + "song title, and a brief description hyping your request."
+				sendMessage(s, messageTemp)
+				
+			#When !bot command is typed in chat, will print the URL for the bot's chat.	
+			if "!bot" in message:
+				messageTemp = "Hi, I'm BraveNewFavesBot. Check out my chat window to see all tonight's requests: "
+				messageTemp = messageTemp + "https://www.twitch.tv/popout/bravenewfavesbot/chat"
+				sendMessage(s, messageTemp)
+				
+			#When command !results is typed, will print the URL for the results page for the BNF VCAs compiled by Tantusar 	
+			if "!results" in message:
+				messageTemp = "Check out the results for the Brave New Faves 2018 Viewers' Choice Awards at "
+				messageTemp = messageTemp + "https://bravenewfaves.github.io/ "
+				sendMessage(s, messageTemp)
+				
+			#When command !spotify is typed, will post link to spotify playlist of Kathleen's choices as compiled by pterodactal
+			if "!spotify" in message:
+				sendMessage(s, spotifylist)
+				
+			#When command !wiki is typed, will post link to the BNF wiki
+			if "!wiki" in message:
+				messageTemp = "Past playlists can be found on the BNF wiki: "
+				messageTemp = messageTemp + "https://wiki.loadingreadyrun.com/index.php/Brave_New_Faves"
+				sendMessage(s, messageTemp)
+				
+			#I should combine these last few blocks into just one section of "mod" commands, and have
+			# everything above in "user" commands. Some other time, perhaps.
+			
+			#Once you select a song to be played, type !nowplaying X into the chat, where X is the number
+			# listed before the request in the bot's chat / the reqlist file. This needs some better error checking.
+			#Now any user can type !nowplaying to get the current song, but if I do it my bot breaks. :(
+			if ("!nowplaying" in message):
+				if ("kathleen_lrr" in user or "freshprinceofbeleren" in user):
+					index = int(message[11:])-1
+					if (index >= 0 and index < len(lst)):
+						playedlst.append(lst[index])
+						sendMessage(s, "Now playing request #" + lst[index])
+					else:
+						sendMessage(s, "Could not find request")
+				else:
+					if (len(playedlst) < 1):
+						sendMessage(s, "No requests have been played yet")
+					else:
+						sendMessage(s, "Now playing request #" + playedlst[len(playedlst)])
+				break			
+			
+			#If you type in the command !printlist, the bot will literally print
+			# the list to an output file every time. It writes over older versions of the file,
+			# but since the list keeps older posts between printings, it's like adding to the file. 
+			#For now I have my username in here too since I'm the one who is running the code,
+			# but I will remove it once Kathleen takes over.
+			#I used this format for the command to mirror commands in other popular chat bots.
+			if ("kathleen_lrr" in user or "freshprinceofbeleren" in user) and "!printlist" in message:
+				with io.open(reqlist, 'w', encoding="utf-8") as f:
+					for line in lst:
+						f.write(("%s\n\n" % line))
+					f.close()
+				break
+			
+			#If you type in the command !printplaylist, the bot will literally print 
+			# the list of songs played to an output file every time. It writes over older versions of 
+			# the file, but since the list keeps older items in the queue between printings, it's like 
+			# adding to the file.
+			if ("kathleen_lrr" in user or "freshprinceofbeleren" in user) and "!printplaylist" in message:
+				with io.open(playlist, 'w', encoding="utf-8") as f:
+					for line in playedlst:
+						f.write(("%s\n\n" % line))
+					f.close()
+				break
+				
+			#This lets pterodactal set the spotify playlist link for Kathleen's picks.
+			if ("pterodactal" in user and "!set_playlist" in message):
+				spotifylist = "Tonight's playlist can be found here: "
+				spotifylist += message[13:]
+				
+			
+				
+				
+				
+				
+				
